a4af12f38c5a15430de0293671d44201cb7afe5b
faseeh <faseeh.shahzad@devsinc.com>
2021-09-29 16:12:57 +0500

    Implemented ADvanced Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid Email / Password Combination!'
@@ -14,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in
     redirect_to root_url
   end
 end
